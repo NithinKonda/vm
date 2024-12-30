7350af5e36087627733a1881dc5c3e1e17de94bb
NithinKonda <nithinkonda142@gmail.com>
2024-12-30 21:33:35 +0530

    add Registers enum and implement Machine struct with initialization

--- src/vm.rs
+++ src/vm.rs
@@ -1,4 +1,18 @@
+enum Registers {
+    A,B,C,M,SP,PC,BP,FLAGS
+}
+
+
 struct Machine {
     registers : [u16; 8],
     memory : [u8; 5000],
+}
+
+impl Machine {
+    pub fn new() -> Self {
+        Self {
+            register : [0;8],
+            memory : [0;5000],
+        }
+    }
 }
