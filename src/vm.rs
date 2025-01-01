07b8c6bc1dc65463c91f4b3f7535b3c20529610e
NithinKonda <nithinkonda142@gmail.com>
2025-01-02 00:29:02 +0530

    refactor Addressable trait methods to use mutable self and improve copy logic

--- src/vm.rs
+++ src/vm.rs
@@ -20,24 +20,25 @@ trait Addressable {
         None
     }
 
-    fn write2(&self, addr: u16, value: u16) -> bool {
+    fn write2(&mut self, addr: u16, value: u16) -> bool {
         let lower = value & 0xff;
         let upper = (value & 0xff00) >> 8;
 
-        self.write(addr, lower) && self.write(addr+1, upper)
+        self.write(addr, lower as u8) && self.write(addr+1, upper as u8)
     }
 
 
-    fn copy(&self, from : u16, to : u16, n: usize) -> bool {
+    fn copy(&mut self, from : u16, to : u16, n: usize) -> bool {
         for i in 0..n {
-            if let Some(x) = self.read(from+i) {
-                if !self.write(to+i, x) {
+            if let Some(x) = self.read(from+(i as u16)) {
+                if !self.write(to+(i as u16), x) {
                     return false;
                 }
             } else {
                 return false;
             }
-        }        
+        }    
+        true    
     }
 }
 
@@ -61,13 +62,13 @@ impl LinearMemory {
 impl Addressable for LinearMemory {
     
     fn read(&self, addr : u16) -> Option<u8> {
-        if addr as usize >= self.bytes.len() {
+        if addr as usize >= self.size {
             return None;
         }
         Some(self.bytes[addr as usize])
     }
     fn write(&mut self, addr : u16, value : u8) -> bool {
-        if addr as usize >= self.bytes.len() {
+        if addr as usize >= self.size {
             return false;
         }
         self.bytes[addr as usize] = value;
