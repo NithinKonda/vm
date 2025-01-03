fcd8caef32481cce68865b08511487fc081acb20
NithinKonda <nithinkonda142@gmail.com>
2025-01-04 00:09:03 +0530

    feat: add Register enum with from_u8 method for register conversion

--- src/vm.rs
+++ src/vm.rs
@@ -1,9 +1,26 @@
 use crate::memory::{Addressable, LinearMemory};
 
-
+#[repr(u8)]
 #[derive(Debug)]
 pub enum Register {
     A,B,C,M,SP,PC,BP,FLAGS
+}
+
+
+impl Register {
+    pub fn from_u8(v:u8) -> Option<Self> {
+        match v {
+            x if x == Register::A as u8 => Some(Register::A),
+            x if x == Register::B as u8 => Some(Register::B),
+            x if x == Register::C as u8 => Some(Register::C),
+            x if x == Register::M as u8 => Some(Register::M),
+            x if x == Register::SP as u8 => Some(Register::SP),
+            x if x == Register::PC as u8 => Some(Register::PC),
+            x if x == Register::BP as u8 => Some(Register::BP),
+            x if x == Register::FLAGS as u8 => Some(Register::FLAGS),
+            _ => None,
+        }
+    }
 }
 
 #[repr(u8)]
