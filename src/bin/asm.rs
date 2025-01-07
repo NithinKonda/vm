a9d598ff15cf91258f3a34808d2c2b9e2c807d26
NithinKonda <nithinkonda142@gmail.com>
2025-01-07 22:36:37 +0530

    feat: add command-line argument handling and file opening in main function

--- src/bin/asm.rs
+++ src/bin/asm.rs
@@ -1,5 +1,16 @@
-
+use std::path::Path;
+use std::{env,  fs::File};
 
 fn main() {
-    
+    let args : Vec<_> = env:args().collect();
+    if args.len() != 2 {
+        panic!("usage : {} <input> ", args[0]);
+    }
+
+    let input_file = &args[1];
+
+    let file = match File::open(Path::new(&args[1])) {
+        Err(e) => panic!("failed to open file : {}", e),
+        Ok(file) => file,
+    };
 }
