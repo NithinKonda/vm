ae8791998dd3a91a08c5e166145be6470ac07d8a
NithinKonda <nithinkonda142@gmail.com>
2025-01-07 23:45:30 +0530

    fix: filter out empty tokens when parsing lines in main function

--- src/bin/asm.rs
+++ src/bin/asm.rs
@@ -19,7 +19,7 @@ fn main() -> Result<(), String> {
     let mut output: Vec<u8> = Vec::new();
     for line in BufReader::new(file).lines() {
         let line_inner = line.map_err(|x | format!("failed to read line : {}",x) )?;
-        for t in line_inner.split(" ").filter(|x| x.len() ==0) {
+        for t in line_inner.split(" ").filter(|x| x.len() > 0) {
             let b = u8 ::from_str_radix(t,16).map_err(|x| format!("failed to parse : {}",x))?;
             output.push(b);
         }
