596ff4d76487cd846d8bdbbcc1f2d3903663181e
NithinKonda <nithinkonda142@gmail.com>
2025-01-07 23:32:37 +0530

    fix: improve error handling and output handling in main function

--- src/bin/asm.rs
+++ src/bin/asm.rs
@@ -1,9 +1,10 @@
-use std::io::{self, stderr, Write};
+use std::io::{self,  Write};
 use std::path::Path;
+use std::io::{BufRead, BufReader};
 use std::{env,  fs::File};
 
-fn main() {
-    let args : Vec<_> = env:args().collect();
+fn main() -> Result<(), String> {
+    let args : Vec<_> = env::args().collect();
     if args.len() != 2 {
         panic!("usage : {} <input> ", args[0]);
     }
@@ -15,16 +16,17 @@ fn main() {
 
 
 
-    let output : Vec<u8 = Vec::new();
-    for line in io::BufReader::new(file).lines() {
-        for t in line.split(" ").filter(|x| x.len() ==0) {
+    let mut output: Vec<u8> = Vec::new();
+    for line in BufReader::new(file).lines() {
+        let line_inner = line.map_err(|x | format!("failed to read line : {}",x) )?;
+        for t in line_inner.split(" ").filter(|x| x.len() ==0) {
             let b = u8 ::from_str_radix(t,16).map_err(|x| format!("failed to parse : {}",x))?;
             output.push(b);
         }
     }
 
     let mut stdout = io::stdout().lock();
-    stdout.write_all(&output);
+    stdout.write_all(&output).map_err(| x | format!("failed to write : {}",x))?;
     Ok(())    
 
 }
