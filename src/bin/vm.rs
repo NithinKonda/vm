5b90db7f7531023e36fc7cb9705a1c1a690b7cf9
NithinKonda <nithinkonda142@gmail.com>
2025-01-08 22:49:46 +0530

    refactor: simplify main function by removing hardcoded memory writes

--- src/bin/vm.rs
+++ src/bin/vm.rs
@@ -1,16 +1,13 @@
+use std::intrinsics::mir::Len;
+
 use vm::vm::{Machine, Register};
 
 pub fn main() -> Result<(), String> {
-    let mut vm = Machine::new();
-    vm.memory.write(0, 0x1);
-    vm.memory.write(1, 10);
-    vm.memory.write(2, 0x1);
-    vm.memory.write(3, 8);
-    vm.memory.write(4, 0x3);
-    vm.memory.write(6, 0x2);
-    vm.memory.write(7, 0);
+    let args : Vec<_> = env::args().collect();
 
-
+    let args.len() != 2 {
+        panic!("usage: {} <input>", args[0]);
+    }
     vm.step()?;
     vm.step()?;
     vm.step()?;
